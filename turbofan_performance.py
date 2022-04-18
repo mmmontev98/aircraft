2f4032d8b84ffafd43b994b846a205dbcfbb4016
Matheus Monteverde <mmmontev98@gmail.com>
2022-04-18 11:18:38 -0300

    minor changes

--- turbofan_performance.py
+++ turbofan_performance.py
@@ -8,24 +8,21 @@ from calculation_modules.new_propulsion.propulsion_wrapper import PropulsionWrap
 
 def main():
     ''' Component Parameters'''
-    aircraft_name = 'TurboJet21'
-    # aircraft_name = 'TurboFan23'
-    # aircraft_name = 'TurboFan22'
-    # aircraft_name = 'RamJet21'
+    aircraft_name = 'TurboFan22'
 
     ''' Analysis Parameters'''
-    list_of_mach = [0, 0.85, 2.00]
-    list_of_Pa = [101.30, 18.75, 7.170]
-    list_of_Ta = [288.2, 216.7, 216.7]
+    list_of_mach = [0, 0.85]
+    list_of_Pa = [101.30, 18.75]
+    list_of_Ta = [288.2, 216.7]
     N_2 = 1
-    list_of_pressure_ratio = np.linspace(5, 100, 40)
+    list_of_air_passage_ratio = np.linspace(0, 14, 40)
     list_of_combustor_outlet_temperature = [1300, 1500, 1700]
     rotation_flag = False
 
     aircraft = load_aircraft(aircraft_name)
     new_propulsion_module = aircraft.get_new_propulsion_module()
     
-    compressor = new_propulsion_module.get_component(1, 'compressor')
+    fan = new_propulsion_module.get_component(1, 'fan')
     combustor = new_propulsion_module.get_component(1, 'combustor')
 
     fig1 = plt.figure(figsize=(1,3))
@@ -37,9 +34,9 @@ def main():
     fig3 = plt.figure()
     fig3.suptitle("list_of_thermal_efficiency")
     for k, (mach, Pa, Ta) in enumerate(zip(list_of_mach, list_of_Pa, list_of_Ta)):
-        axis1 = fig1.add_subplot(1,3,k+1)
-        axis2 = fig2.add_subplot(1,3,k+1)
-        axis3 = fig3.add_subplot(1,3,k+1)
+        axis1 = fig1.add_subplot(1,2,k+1)
+        axis2 = fig2.add_subplot(1,2,k+1)
+        axis3 = fig3.add_subplot(1,2,k+1)
         Po = Pa
 
         for combustor_outlet_temperature in list_of_combustor_outlet_temperature:
@@ -48,8 +45,8 @@ def main():
             list_of_specific_thrust = []
             list_of_thermal_efficiency = []
             list_of_propulsion_efficiency = []
-            for pressure_ratio in list_of_pressure_ratio:
-                compressor.set_pressure_ratio(pressure_ratio)
+            for air_passage_ratio in list_of_air_passage_ratio:
+                fan.set_air_passage_ratio(air_passage_ratio)
                 propulsion_wrapper = set_propulsion_parameters(aircraft, mach, Pa, Po, Ta, N_2, rotation_flag)
                 propulsion_wrapper.initialize()
                 propulsion_wrapper.compute()
@@ -62,18 +59,18 @@ def main():
 
             
             
-            axis1.plot(list_of_pressure_ratio, list_of_specific_thrust, marker='o', label = combustor_outlet_temperature)
-            axis1.set_ylim([0, 1200])
+            axis1.plot(list_of_air_passage_ratio, list_of_specific_thrust, marker='o', label = combustor_outlet_temperature)
+            axis1.set_ylim([0, 2600])
             axis1.set_title("m = {}, T_a = {}, P_a = {}".format(mach, Ta, Pa))
             axis1.legend()
 
             
-            axis2.plot(list_of_pressure_ratio, list_of_TSFC, marker='o', label = combustor_outlet_temperature)
-            axis2.set_ylim([0, 0.00006])
+            axis2.plot(list_of_air_passage_ratio, list_of_TSFC, marker='o', label = combustor_outlet_temperature)
+            axis2.set_ylim([0, 0.00003])
             axis2.set_title("m = {}, T_a = {}, P_a = {}".format(mach, Ta, Pa))
             axis2.legend()
             
-            axis3.plot(list_of_pressure_ratio, list_of_thermal_efficiency, marker='o', label = combustor_outlet_temperature)
+            axis3.plot(list_of_air_passage_ratio, list_of_thermal_efficiency, marker='o', label = combustor_outlet_temperature)
             axis3.set_ylim([0, 0.7])
             axis3.set_title("m = {}, T_a = {}, P_a = {}".format(mach, Ta, Pa))
             axis3.legend()
